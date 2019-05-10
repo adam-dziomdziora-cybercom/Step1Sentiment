8810f86beb4fe6648ba8777e02b3f0b340257f4c
Adam Dziomdziora <190140@edu.p.lodz.pl>
2019-05-10 12:23:27 +0200

    split data

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using Microsoft.ML;
+using Step1Sentiment.Models;
+using static Microsoft.ML.DataOperationsCatalog;
 
 namespace Step1Sentiment {
     class Program {
@@ -8,10 +11,30 @@ namespace Step1Sentiment {
         static readonly string[] _dataNames = { "yelp_labelled.txt", "imdb_labelled.txt", "amazon_labelled.txt" };
         static readonly string _dataPath = Path.Combine (Environment.CurrentDirectory, "Data", _dataNames[0]);
         static readonly string _modelPath = Path.Combine (Environment.CurrentDirectory, "Data", "Model.zip");
-       
+
         static void Main (string[] args) {
-            var content = File.ReadLines (_dataPath).ToList();
-            Console.WriteLine(content[0]);
+            MLContext mlContext = new MLContext ();
+            TrainTestData splitDataView = LoadData (mlContext);
+        }
+
+        public static TrainTestData LoadData (MLContext mlContext) {
+            // Load from text file
+            IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentData> (_dataPath, hasHeader : false);
+
+            // Split for test and train sets
+            TrainTestData splitDataView = mlContext.Data.TrainTestSplit (dataView, testFraction : 0.2);
+
+            // DEBUG the splitted data
+            var debugTrainSet = splitDataView.TrainSet.Preview (maxRows: 1000);            
+            var debugTestSet = splitDataView.TestSet.Preview (maxRows: 1000);
+
+            var testSetCount = debugTestSet.RowView.Count();
+            var trainSetCount = debugTrainSet.RowView.Count ();
+
+            Console.WriteLine ($"Training set: {trainSetCount}, test set: {testSetCount}");
+            //
+
+            return splitDataView;
         }
     }
 }
